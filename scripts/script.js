81f0f8ee8a809c32d1c38f1d99fdaf537a282291
Bobbythagod <rdslaughter96@gmail.com>
2023-04-17 18:46:44 -0500

    Ch 10

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// Author: Robert Slaughter
+// File Name: script.js
+// Date: 04/17/2023
+
+// Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
